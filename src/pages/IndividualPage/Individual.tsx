54dbd766c037f07a5b68cf89e3c93abb2d94f0df
HyunJoo Kim <hyunjoogo@gmail.com>
2023-10-21 22:51:52 +0900

    #9 레이아웃 다시 만들기

--- src/pages/IndividualPage/Individual.tsx
+++ src/pages/IndividualPage/Individual.tsx
@@ -1,72 +1,114 @@
-import React, { useCallback, useState,useRef }  from 'react';
+import React, { useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { ROUTES_PATH } from '@Constants/routes';
-import PhotoUpload from './PhotoUpload';
 import styled from 'styled-components';
-import image from './image.jpeg';
+import useImageUpload from '@Pages/IndividualPage/hooks/useImageUpload';
+import People from '@Assets/icons/People';
+import color from '@Styles/color';
+import Down from '@Assets/icons/Down';
+import sampleImg from './image.jpeg';
 
 const Individual = () => {
-  const UseImageUpload = () => {
-    const [file, setFile] = useState<FileList | null>(null);
-    const [imgURL, setImgURL] = useState("");
-  
-    const selectImg = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
-      if (e.target.files) {
-        setFile(e.target.files);
-  
-        const newImgURL = URL.createObjectURL(e.target.files[0]);
-        setImgURL(newImgURL);
-      }
-    }, []);
-  
-    return { file, imgURL, selectImg  };
-  };
-  
+  const { file, imgURL, selectImg, onSubmit } = useImageUpload();
   const [showPhotoUpload, setShowPhotoUpload] = useState(false);
   const imgUploadInput = useRef<HTMLInputElement | null>(null);
 
-  const { file, imgURL, selectImg } = UseImageUpload();
+  const handleImgClick = () => {
+    imgUploadInput.current?.click();
+  };
 
   return (
-    <AppContainer>
-      <BodyContainer>
-        <Header>
-          <BackLink to={ROUTES_PATH.home}>뒤로</BackLink>
-          <RightLink>
-            <StyledLink to={ROUTES_PATH.home}>초대</StyledLink>
-            <StyledLink to={ROUTES_PATH.home}>저장</StyledLink>
-          </RightLink>
-        </Header>
-        <StyledDiv>
-          {imgURL === "" ?
-            <StyledAlert>
-              <Stlyedinfo>사진이 없어요ㅠ</Stlyedinfo>
-              <StyledImg src={image} alt="You don't have photo!"/>
-            </StyledAlert>
-          :
-          <StyledImg src={imgURL} alt="Upload Photo"/>
-        }
-        </StyledDiv>
-      </BodyContainer>
-
-      <ImgInput
-        type="file"
-        accept="image/*"
-        required
-        ref = {imgUploadInput}
-        onChange = {selectImg} />
-
-      <AddButton type="button" onClick={(e) => {
-          e.preventDefault();
-          if (imgUploadInput.current) {
-            imgUploadInput.current.click();
-          }}}> 이미지 변경 버튼 </AddButton>
-
-      <ButtonContainer>
-      </ButtonContainer>
-    </AppContainer>
+    <Layout>
+      <Header>
+        <People color={color.gray[700]} />
+        <Down color={color.gray[700]} />
+      </Header>
+      <Content>
+        {/* 사진이 아무것도 없을때 */}
+        <input
+          type="file"
+          accept="image/*"
+          required
+          ref={imgUploadInput}
+          onChange={selectImg}
+          style={{ display: 'none' }}
+        />
+        {/* 기본이미지일 때는 마우스호버하면 손모양, 이미지가 있으면 그냥 마우스 */}
+        <img src={imgURL} onClick={handleImgClick} />
+      </Content>
+      <Button onClick={onSubmit}>사진 올리기</Button>
+    </Layout>
   );
 };
+
+const Layout = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+const Header = styled.div`
+  width: 100%;
+  height: 106px;
+  background-color: aqua;
+`;
+
+const Content = styled.div`
+  width: 100%;
+  //height: 80%;
+  background-color: black;
+`;
+
+const Button = styled.button`
+  width: 100%;
+  height: 107px;
+  padding: 20px 0;
+  background-color: ${color.primary};
+`;
+
+const Img = styled.img`
+  &:hover {
+  }
+`;
+
+// <AppContainer>
+//   <BodyContainer>
+//     <Header>
+//       <BackLink to={ROUTES_PATH.home}>뒤로</BackLink>
+//       <RightLink>
+//         <StyledLink to={ROUTES_PATH.home}>초대</StyledLink>
+//         <StyledLink to={ROUTES_PATH.home}>저장</StyledLink>
+//       </RightLink>
+//     </Header>
+//     <StyledDiv>
+//       {imgURL === '' ? (
+//         <StyledAlert>
+//           <Stlyedinfo>사진이 없어요ㅠ</Stlyedinfo>
+//           <StyledImg src={image} alt="You don't have photo!" />
+//         </StyledAlert>
+//       ) : (
+//         <StyledImg src={imgURL} alt="Upload Photo" />
+//       )}
+//     </StyledDiv>
+//   </BodyContainer>
+//
+//   <ImgInput type="file" accept="image/*" required ref={imgUploadInput} onChange={selectImg} />
+//
+//   <AddButton
+//     type="button"
+//     onClick={(e) => {
+//       e.preventDefault();
+//       if (imgUploadInput.current) {
+//         imgUploadInput.current.click();
+//       }
+//     }}
+//   >
+//     {' '}
+//     이미지 변경 버튼{' '}
+//   </AddButton>
+//
+//   <ButtonContainer></ButtonContainer>
+// </AppContainer>
 
 // styled-components
 const AppContainer = styled.div`
@@ -88,38 +130,38 @@ const BodyContainer = styled.div`
 `;
 
 // Header
-const Header = styled.div`
-  width: 375px;
-  height: 52px;
-  display: flex;
-  justify-content: space-between;
-  margin-bottom: auto;
-  border: 2px solid black;
-  border-bottom: none;
-  align-items: center;
-`;
+// const Header = styled.div`
+//   width: 375px;
+//   height: 52px;
+//   display: flex;
+//   justify-content: space-between;
+//   margin-bottom: auto;
+//   border: 2px solid black;
+//   border-bottom: none;
+//   align-items: center;
+// `;
 
 // Header Button
 const BackLink = styled(Link)`
-width: 64px;
-height: 19px;
-padding: 10px;
-border: 2px solid black;
-border-radius: 5px;
-display: flex;
-justify-content: center;
-align-items: center;
-margin-left: 5px;
+  width: 64px;
+  height: 19px;
+  padding: 10px;
+  border: 2px solid black;
+  border-radius: 5px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-left: 5px;
 
-&:hover {
-  background-color: orange;
-}
-`
+  &:hover {
+    background-color: orange;
+  }
+`;
 
 const RightLink = styled.div`
   display: flex;
   align-items: center;
-`
+`;
 
 const StyledLink = styled(Link)`
   width: 64px;
@@ -159,19 +201,19 @@ const Stlyedinfo = styled.div`
   font-size: 25px;
   font-weight: semi-bold;
   margin: 5px;
-`
+`;
 
 const StyledImg = styled.img`
   width: 200px;
   height: 130px;
-`
+`;
 
 const ButtonContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  margin-top: 10px
-`
+  margin-top: 10px;
+`;
 
 const AddButton = styled.button`
   width: 333px;
@@ -185,6 +227,6 @@ const AddButton = styled.button`
 
 const ImgInput = styled.input`
   display: none;
-`
+`;
 
 export default Individual;
